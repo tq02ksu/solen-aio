4470ee1065d62bab32fb05c0030f19dddb028ee1
tq02ksu <donbowu@neusoft.com>
2019-06-26 17:35:32 +0800

    emm.. friendship is first, protocol

--- backend/src/main/java/com/neusoft/solen/controller/MessageController.java
+++ backend/src/main/java/com/neusoft/solen/controller/MessageController.java
@@ -52,7 +52,7 @@ public class MessageController {
         Channel ch = connectionManager.getStore().get(deviceId).getChannel();
         ConnectionManager.Connection conn = connectionManager.getStore().get(deviceId);
         synchronized (ch) {
-            byte[] buffer = new byte[] {(byte) (0x00 + request.getCtrl()), (byte) (0x01 - request.getCtrl()),
+            byte[] buffer = new byte[] {(byte) (request.getCtrl()), (byte) (0x01 - request.getCtrl()),
                     0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
 
             buffer[0] = (byte) request.getCtrl();
