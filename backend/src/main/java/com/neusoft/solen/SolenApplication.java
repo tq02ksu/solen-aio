a50835cb5f8146cc3833c106d5bcd4d46e38758f
tq02ksu <donbowu@neusoft.com>
2019-07-16 21:11:15 +0800

    bugfix for time calculation

--- backend/src/main/java/com/neusoft/solen/SolenApplication.java
+++ backend/src/main/java/com/neusoft/solen/SolenApplication.java
@@ -3,7 +3,9 @@ package com.neusoft.solen;
 import com.neusoft.solen.server.ServerProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ServerProperties.class)
@@ -11,5 +13,10 @@ public class SolenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SolenApplication.class, args);
+	}
+
+	@Bean
+	public Jackson2ObjectMapperBuilderCustomizer dateFormatCustomer() {
+		return builder -> builder.simpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS");
 	}
 }
