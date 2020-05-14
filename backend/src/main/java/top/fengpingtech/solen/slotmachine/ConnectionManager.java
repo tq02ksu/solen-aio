68f2eee34240bae18ca876977981d4a22e5dc6bf
tq02ksu <donbowu@neusoft.com>
2020-05-14 14:09:39 +0800

    remove connection house keeper thread

--- backend/src/main/java/top/fengpingtech/solen/slotmachine/ConnectionManager.java
+++ backend/src/main/java/top/fengpingtech/solen/slotmachine/ConnectionManager.java
@@ -5,16 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import top.fengpingtech.solen.model.Connection;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class ConnectionManager {
     private static final Logger logger = LoggerFactory.getLogger(ConnectionManager.class);
-
-    private Thread thread;
 
     private boolean shutdown = false;
     private final Map<String, Connection> store = new ConcurrentHashMap<>();
@@ -29,45 +25,5 @@ public class ConnectionManager {
         } catch (InterruptedException e) {
             logger.warn("error close conn: {}", conn, e);
         }
-    }
-
-    @PostConstruct
-    public void init() {
-        thread = new Thread(() -> {
-            while (true) {
-                store.forEach((key, val) -> {
-                    try {
-                        if (val.getChannel().isOpen()
-                                && System.currentTimeMillis() - val.getLastHeartBeatTime().getTime() > Connection.HEARTBEAT_TIMEOUT_MS * 2) {
-                            logger.info("deviceId={} last heartbeatTime is {}, seem to lost connection, ticking",
-                                    key, val.getLastHeartBeatTime());
-                            close(val);
-                        }
-                    } catch (Exception e) {
-                        logger.error("error while close channel, deviceId={}", key);
-                    }
-                });
-
-                if (shutdown) {
-                    return;
-                }
-
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    logger.info("interrupted", e);
-                    break;
-                }
-            }
-        });
-        thread.setName("Connection-Guard");
-        thread.setDaemon(true);
-        thread.start();
-    }
-
-    @PreDestroy
-    public void destroy() {
-        shutdown = true;
-        thread.interrupt();
     }
 }
