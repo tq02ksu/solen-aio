df3aa6396c1f9838291e22e31fb524b0b4a1bba4
tq02ksu <donbowu@neusoft.com>
2020-08-16 15:48:33 +0800

    error tolerant short deviceId

--- backend/src/main/java/top/fengpingtech/solen/slotmachine/MessageEncoder.java
+++ backend/src/main/java/top/fengpingtech/solen/slotmachine/MessageEncoder.java
@@ -6,6 +6,8 @@ import io.netty.handler.codec.MessageToByteEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.charset.StandardCharsets;
+
 public class MessageEncoder extends MessageToByteEncoder<SoltMachineMessage> {
     private static final Logger logger = LoggerFactory.getLogger(MessageEncoder.class);
 
@@ -16,12 +18,17 @@ public class MessageEncoder extends MessageToByteEncoder<SoltMachineMessage> {
 
     public void encode(SoltMachineMessage msg, ByteBuf out) {
         logger.info("message sent: " + msg);
+
         int startIndex = out.readerIndex();
         out.writeShortLE(msg.getHeader());
         out.writeShortLE(msg.getData().length + 26);
         out.writeByte(msg.getIndex());
         out.writeLongLE(msg.getIdCode());
         out.writeBytes(msg.getDeviceId().getBytes());
+        for (int i = 0; i < 11 - msg.getDeviceId().getBytes(StandardCharsets.UTF_8).length; i ++) {
+            out.writeByte('0');
+        }
+
         out.writeByte((byte)msg.getCmd());
         out.writeBytes(msg.getData());
 
