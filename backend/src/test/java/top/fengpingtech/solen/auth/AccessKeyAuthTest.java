6cc0920b8e23bbae23b1384c2a794f2aedc2de3e
tq02ksu <donbowu@neusoft.com>
2020-08-18 16:32:41 +0800

    add okhttp appkey/secret test

--- backend/src/test/java/top/fengpingtech/solen/auth/AccessKeyAuthTest.java
+++ backend/src/test/java/top/fengpingtech/solen/auth/AccessKeyAuthTest.java
@@ -1,5 +1,9 @@
 package top.fengpingtech.solen.auth;
 
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
@@ -10,13 +14,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class AccessKeyAuthTest extends SolenApplicationTests {
     private String appKey = "test";
-    private String secretKey = "$2a$10$thdy4TLr809cq6kpHdT1JOeTS.IiZfVFnjfkuxgqs.cSj53ffCWV.";
+    private String secretKey = "test";
     private String passKey = "test";
 
     @Autowired
     MockMvc mockMvc;
 
-    String sign (String url, Long requestTime) {
+    private String sign(String url, Long requestTime) {
         String msg = String.format("%s%s%s%s", secretKey, requestTime, url, secretKey);
         return new AppKeySignatureChecker().digest(msg);
     }
@@ -29,5 +33,25 @@ public class AccessKeyAuthTest extends SolenApplicationTests {
                 .param("appKey", appKey)
                 .param("sign", sign("/api/list", requestTime))
         ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testRemoteServer() throws Exception {
+        String appKey = "test", secretKey = "test";
+        Long requestTime = System.currentTimeMillis() / 1000;
+        String sign = sign("/api/list", requestTime);
+
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        Request request = new Request.Builder()
+                .header("Content-Type", "application/json")
+                .get()
+                .url(HttpUrl.parse("http://iot.fengping-tech.top/api/list").newBuilder()
+                        .addQueryParameter("requestTime", String.valueOf(requestTime))
+                        .addQueryParameter("appKey", appKey)
+                        .addQueryParameter("sign", sign)
+                        .build())
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
     }
 }
