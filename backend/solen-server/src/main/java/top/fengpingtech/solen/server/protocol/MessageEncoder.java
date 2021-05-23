aa46a93dd42aceaa38ed2a7163cb3b6c1c5646cd
tq02ksu <donbowu@neusoft.com>
2021-05-23 10:08:13 +0800

    fix bug for message encoder

--- backend/solen-server/src/main/java/top/fengpingtech/solen/server/protocol/MessageEncoder.java
+++ backend/solen-server/src/main/java/top/fengpingtech/solen/server/protocol/MessageEncoder.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import top.fengpingtech.solen.server.model.SoltMachineMessage;
 
+import java.nio.charset.StandardCharsets;
+
 public class MessageEncoder extends MessageToByteEncoder<SoltMachineMessage> {
     private static final Logger logger = LoggerFactory.getLogger(MessageEncoder.class);
 
@@ -26,5 +28,26 @@ public class MessageEncoder extends MessageToByteEncoder<SoltMachineMessage> {
     }
 
     public void encode(SoltMachineMessage msg, ByteBuf out) {
+        logger.info("message sent: " + msg);
+
+        int startIndex = out.readerIndex();
+        out.writeShortLE(msg.getHeader());
+        out.writeShortLE(msg.getData().length + 26);
+        out.writeByte(msg.getIndex());
+        out.writeLongLE(msg.getIdCode());
+        out.writeBytes(msg.getDeviceId().getBytes());
+        for (int i = 0; i < 11 - msg.getDeviceId().getBytes(StandardCharsets.UTF_8).length; i++) {
+            out.writeByte('0');
+        }
+
+        out.writeByte(msg.getCmd());
+        out.writeBytes(msg.getData());
+
+        byte checksum = 0;
+        for (int i = 0; i < msg.getData().length + 26 - 1; i++) {
+            checksum ^= out.readByte();
+        }
+        out.readerIndex(startIndex);
+        out.writeByte(checksum);
     }
 }
