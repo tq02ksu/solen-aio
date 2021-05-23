75dd4b87b79d382413e0ea962efa7cb457db8e81
tq02ksu <donbowu@neusoft.com>
2021-05-23 11:10:18 +0800

    add ToString annotation

--- backend/solen-server/src/main/java/top/fengpingtech/solen/server/model/LocationEvent.java
+++ backend/solen-server/src/main/java/top/fengpingtech/solen/server/model/LocationEvent.java
@@ -2,9 +2,11 @@ package top.fengpingtech.solen.server.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class LocationEvent extends Event {
     private String imei;
 
--- backend/solen-server/src/main/java/top/fengpingtech/solen/server/model/MessageEvent.java
+++ backend/solen-server/src/main/java/top/fengpingtech/solen/server/model/MessageEvent.java
@@ -2,9 +2,11 @@ package top.fengpingtech.solen.server.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class MessageEvent extends Event {
     private String message;
 }
