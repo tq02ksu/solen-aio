18008e22175fcee3af4d2c15c859d76d8aa8a20f
dongbowu <dongbowu@neusoft.com>
2022-06-10 23:53:07 +0800

    添加sql init 脚本

--- backend/solen-app/src/main/resources/schema.sql
+++ backend/solen-app/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 create table if not exists device (
     device_id varchar(32) not null primary key , -- comment '设备ID'
-    status tinyint not null , -- default 0comment '状态, '
+    status varchar(32) not null , -- default 0comment '状态, '
     lac int not null, --  default 0comment '基站lac'
     ci int not null, -- default 0 comment '基站ci'
     id_code varchar(32) not null, -- comment '设备id code',
@@ -10,7 +10,7 @@ create table if not exists device (
     voltage double not null, -- default 0.0, -- comment '电压',
     temperature double not null, -- default 0.0, -- comment '',
     gravity tinyint not null, -- default 0, -- comment '重力感应',
-    uptime, int not null -- default 0 -- comment 开机时间
+    uptime int not null, -- default 0 -- comment 开机时间
     lat double not null, -- default 0 comment '纬度'
     lng double not null -- default 0 comment '经度'
 ); -- comment '设备' ;
@@ -21,7 +21,7 @@ create table if not exists connection (
   device_id varchar(32) not null -- comment '设备id',
 );
 
-create index idx_device_id on connection ( device_id );
+create index if not exists idx_connection_device_id on connection ( device_id );
 
 create cached table if not exists event (
   event_id varchar(32) not null primary key,
@@ -31,5 +31,5 @@ create cached table if not exists event (
   details varchar(1024) not null
 );
 
-create index idx_device_id on event ( device_id );
-create index idx_event_type on event ( event_type );
+create index if not exists idx_event_device_id on event ( device_id );
+create index if not exists idx_event_type on event ( type );
