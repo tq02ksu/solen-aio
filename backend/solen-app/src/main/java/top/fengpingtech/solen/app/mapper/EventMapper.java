9205e71e924048abd84a1f7ed1f6aab7045fa6db
tq02ksu <tq02ksu@gmail.com>
2026-05-24 18:46:12 +0800

    Add mapping for deviceId in EventMapper

--- backend/solen-app/src/main/java/top/fengpingtech/solen/app/mapper/EventMapper.java
+++ backend/solen-app/src/main/java/top/fengpingtech/solen/app/mapper/EventMapper.java
@@ -2,12 +2,14 @@ package top.fengpingtech.solen.app.mapper;
 
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import top.fengpingtech.solen.app.controller.bean.EventBean;
 import top.fengpingtech.solen.app.domain.EventDomain;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface EventMapper {
+    @Mapping(target = "deviceId", source = "device.deviceId")
     EventBean mapToBean(EventDomain event);
 
     List<EventBean> mapToBean(List<EventDomain> event);
